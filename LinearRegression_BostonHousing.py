fe6c8e1c65dc3b9ad6172ace81e166d8e18aec23
Andres Zamora <andreszamora1998@gmail.com>
2020-04-25 18:44:28 -0500

    Ver precision del modelo

--- LinearRegression_BostonHousing.py
+++ LinearRegression_BostonHousing.py
@@ -1,55 +1,64 @@
+#Se importan la librerias a utilizar
 import numpy as np
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
-
-#Importando Datos de la Libreria
+########## PREPARAR LA DATA ##########
+#Importamos los datos de la misma librería de scikit-learn
 boston = datasets.load_boston()
 print(boston)
 print()
-
-#Entendiendo la Data
-print('Informacion en el DataSet')
+########## ENTENDIMIENTO DE LA DATA ##########
+#Verifico la información contenida en el dataset
+print('Información en el dataset:')
 print(boston.keys())
 print()
-
-#Verifico las Caracteristicas del dataset
-print('Caracteristicas del dataset:')
+#Verifico las características del dataset
+print('Características del dataset:')
 print(boston.DESCR)
-
-#Verifico la Cantidad de datos que hay en los dataset
+#Verifico la cantidad de datos que hay en los dataset
 print('Cantidad de datos:')
 print(boston.data.shape)
 print()
-
-#Verifico la informacion de las columnas
+#Verifico la información de las columnas
 print('Nombres columnas:')
 print(boston.feature_names)
-
-#Preparar la data regresion lineal simple
-X=boston.data[:,np.newaxis,5]
-#ddefino los datos correspondientes a las etiquetas
-y=boston.target
-
-#Graficamos los datos correspondientes a las etiquetas
-plt.scatter(X,y)
-plt.xlabel('Numero de habitaciones')
+########## PREPARAR LA DATA REGRESIÓN LINEAL SIMPLE ##########
+#Seleccionamos solamente la columna 5 del dataset
+X = boston.data[:, np.newaxis, 5]
+#Defino los datos correspondientes a las etiquetas
+y = boston.target
+#Graficamos los datos correspondientes
+plt.scatter(X, y)
+plt.xlabel('Número de habitaciones')
 plt.ylabel('Valor medio')
 plt.show()
-
-#Implementacion de Regresion Lineal simple
+########## IMPLEMENTACIÓN DE REGRESIÓN LINEAL SIMPLE ##########
 from sklearn.model_selection import train_test_split
-
-#separo los datos de "train" en entrenamiento y prueba para probar los algoritmos
-X_train, X_test,y_train,y_test=train_test_split(X,y,test_size=0.3)
-
+#Separo los datos de "train" en entrenamiento y prueba para probar los algoritmos
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 #Defino el algoritmo a utilizar
-lr=linear_model.LinearRegression()
-
-#Entrenar el Modelo
-lr.fit(X_train,y_train)
-
-#Realizo una Prediccion
-Y_pred=lr.predict(X_test)
-
-print('precision del modelo')
-print(lr.score(X_train,y_train))
+lr = linear_model.LinearRegression()
+#Entreno el modelo
+lr.fit(X_train, y_train)
+#Realizo una predicción
+Y_pred = lr.predict(X_test)
+#Graficamos los datos junto con el modelo
+plt.scatter(X_test, y_test)
+plt.plot(X_test, Y_pred, color='red', linewidth=3)
+plt.title('Regresión Lineal Simple')
+plt.xlabel('Número de habitaciones')
+plt.ylabel('Valor medio')
+plt.show()
+print()
+print('DATOS DEL MODELO REGRESIÓN LINEAL SIMPLE')
+print()
+print('Valor de la pendiente o coeficiente "a":')
+print(lr.coef_)
+print('Valor de la intersección o coeficiente "b":')
+print(lr.intercept_)
+print()
+print('La ecuación del modelo es igual a:')
+print('y = ', lr.coef_, 'x ', lr.intercept_)
+print()
+print('Precisión del modelo:')
+print(lr.score(X_train, y_train))
